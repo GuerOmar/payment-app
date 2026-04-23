d227c3d88c6674adb911dc24d14739f3290c9e6d
GuerOmar <64172023+GuerOmar@users.noreply.github.com>
2026-04-23 10:43:46 +0200

    feat: The displayed payment methods should be associated to the user's payment methods.

--- src/main/java/com/payment/service/payment/PaymentService.java
+++ src/main/java/com/payment/service/payment/PaymentService.java
@@ -1,10 +1,13 @@
 package com.payment.service.payment;
 
 
+import com.payment.model.PaymentMethod;
 import com.payment.model.enums.PaymentMethodType;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public interface PaymentService {
     void processPayment(BigDecimal amount, Long paymentMethodId, PaymentMethodType paymentMethodType);
+    List<PaymentMethod> findAllByUsername(String username);
 }
